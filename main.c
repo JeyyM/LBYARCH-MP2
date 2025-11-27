07dfddcd347a1e56411249c026105c97779f0f61
JeyM <jeymson8000@gmail.com>
2025-11-27 21:21:50 +0800

    Tests added

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 // note unsigned char is the uint8 requirement which is 0 to 255
 extern void imgCvtGrayDoubleToInt(int height, int width, double* floatPixels, unsigned char* intPixels);
@@ -36,5 +37,125 @@ int main() {
         printf("\n");
     }
     
+    getchar();
+
+    // 10x10 test
+
+    height = 10;
+    width = 10;
+    totalPixels = height * width;
+    double randFloatPixels[100];
+    unsigned char randIntPixels[100];
+    int runs = 30;
+    double totalTime = 0.0;
+    srand((unsigned int)time(NULL));
+
+    for (int run = 0; run < runs; run++) {
+        for (int i = 0; i < totalPixels; i++) {
+            randFloatPixels[i] = (double)rand() / RAND_MAX;
+        }
+        clock_t start = clock();
+        imgCvtGrayDoubleToInt(height, width, randFloatPixels, randIntPixels);
+        clock_t end = clock();
+        double elapsed = (double)(end - start) / CLOCKS_PER_SEC;
+        totalTime += elapsed;
+
+        if (run == 0) {
+            printf("\nFirst 10x10 input:\n");
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    printf("%.2f ", randFloatPixels[i * width + j]);
+                }
+                printf("\n");
+            }
+            printf("\nFirst 10x10 output:\n");
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    printf("%3u ", (unsigned char)randIntPixels[i * width + j]);
+                }
+                printf("\n");
+            }
+        }
+    }
+    printf("\nAverage time for 10x10: %.8f seconds per run\n", totalTime / runs);
+
+    getchar();
+
+
+    // 100x100 test
+    height = 100;
+    width = 100;
+    totalPixels = height * width;
+    double randFloatPixels100[10000];
+    unsigned char randIntPixels100[10000];
+    double totalTime100 = 0.0;
+
+    for (int run = 0; run < runs; run++) {
+        for (int i = 0; i < totalPixels; i++) {
+            randFloatPixels100[i] = (double)rand() / RAND_MAX;
+        }
+        clock_t start = clock();
+        imgCvtGrayDoubleToInt(height, width, randFloatPixels100, randIntPixels100);
+        clock_t end = clock();
+        double elapsed = (double)(end - start) / CLOCKS_PER_SEC;
+        totalTime100 += elapsed;
+
+        if (run == 0) {
+            printf("\nFirst 100x100 input:\n");
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    printf("%.2f ", randFloatPixels100[i * width + j]);
+                }
+                printf("\n");
+            }
+            printf("\nFirst 100x100 output:\n");
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    printf("%3u ", (unsigned char)randIntPixels100[i * width + j]);
+                }
+                printf("\n");
+            }
+        }
+    }
+    printf("\nAverage time for 100x100: %.8f seconds per run\n", totalTime100 / runs);
+
+    getchar();
+
+    // 460x460 test
+    height = 460;
+    width = 460;
+    totalPixels = height * width;
+    double randFloatPixels460[211600];
+    unsigned char randIntPixels460[211600];
+    double totalTime460 = 0.0;
+    for (int run = 0; run < runs; run++) {
+        for (int i = 0; i < totalPixels; i++) {
+            randFloatPixels460[i] = (double)rand() / RAND_MAX;
+        }
+        clock_t start = clock();
+        imgCvtGrayDoubleToInt(height, width, randFloatPixels460, randIntPixels460);
+        clock_t end = clock();
+        double elapsed = (double)(end - start) / CLOCKS_PER_SEC;
+        totalTime460 += elapsed;
+
+        if (run == 0) {
+            printf("\nFirst 460x460 input:\n");
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    printf("%.2f ", randFloatPixels460[i * width + j]);
+                }
+                printf("\n");
+            }
+            printf("\nFirst 460x460 output:\n");
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    printf("%3u ", (unsigned char)randIntPixels460[i * width + j]);
+                }
+                printf("\n");
+            }
+        }
+    }
+    printf("\nAverage time for 460x460: %.8f seconds per run\n", totalTime460 / runs);
     return 0;
+
 }
